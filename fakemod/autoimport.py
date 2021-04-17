aacc6192fdd791b1499eb726a084d8ddfc0a85fc
Roger D. Serwy <roger.serwy@gmail.com>
2021-04-17 11:11:34 -0500

    autoimport cache improvements

--- fakemod/autoimport.py
+++ fakemod/autoimport.py
@@ -1,5 +1,5 @@
 import importlib
-
+import sys
 
 """
 AutoImport
@@ -13,6 +13,14 @@ lazy import on demand.
 class AutoImport:
     def __init__(self):
         self._cache = {}
+        self._from_sysmodules()
+
+    def _from_sysmodules(self):
+        for name in sys.modules:
+            base, sep, rest = name.partition('.')
+            if sep == '':
+                self._cache.setdefault(
+                    name, sys.modules[name])
 
     def __getattr__(self, name):
         if name not in self._cache:
@@ -21,4 +29,5 @@ class AutoImport:
         return self._cache[name]
 
     def __dir__(self):
+        self._from_sysmodules()
         return sorted(self._cache)
